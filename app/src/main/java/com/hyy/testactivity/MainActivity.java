b752f5c0856577487a31a5cd615d4e02d9f42cb2
Hyynice <2621032682@qq.com>
2020-06-26 16:19:11 +0800

    1

---
+++ app/src/main/java/com/hyy/testactivity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hyy.testactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("两个幼稚鬼");
+    }
+}
